6353be710113e9701917e79dae9eb3c98ba210ba
João Henrique <joaohcorrales@gmail.com>
2022-12-06 14:27:41 -0300

    Atualizações nas imagens e hospedando no GitHub

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ menu.addEventListener('click', () => {
 })
 
 // BOTÕES E EVENTOS DE CLICK
+
+    const button = document.querySelector('.read-more-button')
+    const hiddenText = document.querySelector('.read-more-text')
+
+    button.addEventListener('click', event => {
+        hiddenText.classList.toggle('read-more-text--show')
+
+        button.textContent = button.textContent.includes('Mostrar mais...')
+        ? 'Mostrar menos...' 
+        : 'Mostrar mais...'
+    })
